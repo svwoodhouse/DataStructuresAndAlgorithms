4ca3a6ce318d7249376e124efa631d6e4ca99caf
svwoodhouse <svwoodhouse@gmail.com>
2019-11-05 23:28:54 -0500

    Added skeleton for adding Nodes function

--- Data Structures/LinkedList/LinkedListExample/src/linkedlist/LinkedList.java
+++ Data Structures/LinkedList/LinkedListExample/src/linkedlist/LinkedList.java
@@ -60,7 +60,13 @@ public class LinkedList
             return n;
         }
     }
-    
+    // You are given two non-empty linked lists representing two non-negative integers. 
+    // The digits are stored in reverse order and each of their nodes contain a single digit. 
+    // Add the two numbers and return it as a linked list.
+    // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    public void addNodes(Node node1,Node node2, int num)
+    {
+    }
     // This function prints the nodes 
     public void printNodes()
     {
