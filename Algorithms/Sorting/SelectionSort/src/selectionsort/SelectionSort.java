5e8594c7fe2ac91f6a54312d152babcc0a0c4baf
svwoodhouse <svwoodhouse@gmail.com>
2019-12-10 18:57:31 -0500

    Added reverse Selection Sort function that sorts the array in decreasing order

--- Algorithms/Sorting/SelectionSort/src/selectionsort/SelectionSort.java
+++ Algorithms/Sorting/SelectionSort/src/selectionsort/SelectionSort.java
@@ -10,12 +10,12 @@ public class SelectionSort
         System.out.println(Arrays.toString(unsortedArray));
         selectionSort(unsortedArray);
         System.out.println(Arrays.toString(unsortedArray));
+        reverseSelectionSort(unsortedArray);
+        System.out.println(Arrays.toString(unsortedArray));
     }
     
     public static void selectionSort(int array[])
     {
-        
-        
         for(int i = array.length-1; i > 0; i--)
         {
             int highestNum = 0;
@@ -32,4 +32,22 @@ public class SelectionSort
         }
     }
     
+    // This fucntion sorts the array in decreasing order using the Selection Sort Algorithm
+    public static void reverseSelectionSort(int[] array)
+    {
+      int lowest = 0;
+      
+      for(int i = array.length-1; i > 0; i--) 
+      {
+          for(int j = 1; j <= i; j++)
+          {
+              if(array[j] < array[lowest])
+                  lowest = j;
+          }
+          int temp = array[i];
+          array[i] = array[lowest];
+          array[lowest] = temp;
+          lowest = 0;
+      }
+    }   
 }
