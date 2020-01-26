14337698bfa9ebcab854482b98a55e14cb62db90
svwoodhouse <svwoodhouse@gmail.com>
2020-01-25 22:31:41 -0500

    Added example of stack implementation

--- Data Structures/Stack/StackExample/src/stackexample/ArrayStack.java
+++ Data Structures/Stack/StackExample/src/stackexample/ArrayStack.java
@@ -34,6 +34,25 @@ public class ArrayStack
        return employee;
     }
     
+    public Employee peek()
+    {
+        if(isEmpty()) {
+            throw new EmptyStackException();
+        }
+        
+        return stack[top -1];
+    }
+    
+    public int size(){
+        return top;
+    }
+    
+    public void printStack() {
+        for(int i = top - 1; i >= 0; i--){
+            System.out.println(stack[i]);
+        }
+    }
+    
     public boolean isEmpty()
     {
         return top == 0;
--- Data Structures/Stack/StackExample/src/stackexample/StackExample.java
+++
@@ -1,9 +0,0 @@
-package stackexample;
-
-public class StackExample 
-{
-    public static void main(String[] args) 
-    {
-        
-    }    
-}
