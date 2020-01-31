f6f260595b499b58d0bf5d3706b5e05085f8c7e6
svwoodhouse <svwoodhouse@gmail.com>
2020-01-30 21:39:21 -0500

    Will finish later

---
+++ Data Structures/HashTable/HashtableChaining/src/hashtablechaining/Main.java
@@ -0,0 +1,26 @@
+package hashtablechaining;
+
+public class Main {
+    public static void main(String[] args) {
+       Employee janeJones = new Employee("Jane", "Jones", 123);
+       Employee johnDoe = new Employee("John", "Doe", 4567);
+       Employee marySmith = new Employee("Mary", "Smith", 22);
+       Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+       ChainedHashTable ht = new ChainedHashTable();
+       ht.put("Jones", janeJones);
+       ht.put("Doe", johnDoe);
+       ht.put("Wilson", mikeWilson);
+       ht.put("Smith", marySmith);
+       
+       ht.printHashTable();
+       
+ //      System.out.println("Retrieve key Smith: " + ht.get("Smith"));
+       
+//       ht.remove("Doe");
+//       ht.remove("Jones");
+//       ht.printHashTable();
+       
+//       System.out.println("Retrieve key Smith: " + ht.get("Smith"));
+    }   
+}
