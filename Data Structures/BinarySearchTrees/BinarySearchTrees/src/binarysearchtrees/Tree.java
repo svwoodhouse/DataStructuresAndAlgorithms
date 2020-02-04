827bb055b2515426dc36c2dd18f5c12ed4b1f03b
Sydnee <Sydnee@LAPTOP-LADAT234>
2020-02-04 18:42:46 -0500

    Adding to repo. Will finish later

---
+++ Data Structures/BinarySearchTrees/BinarySearchTrees/src/binarysearchtrees/Tree.java
@@ -0,0 +1,26 @@
+package binarysearchtrees;
+
+public class Tree {
+    
+    private TreeNode root;
+    
+    public void insert(int value) {
+        if(root == null)
+            root = new TreeNode(value);
+        
+        else
+            root.insert(value);
+    }
+    
+    public void traverseInOrder() {
+        if(root!=null)
+            root.traverseInOrder();
+    }
+    
+    public TreeNode get(int value) {
+        if(root!= null) 
+            return root.get(value);
+        
+        return null;  
+    }
+}
